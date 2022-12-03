a9b5ed5f099727ef5257783bc5cc7f2f1131a958
Alaa Elsayed Alaraby <53160167+alaagawish@users.noreply.github.com>
2022-12-03 23:30:03 +0200

    Create hello.java

---
+++ Lab1/hello.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+class HelloWorld{
+	
+	public static void main(String[] args){
+		
+		
+		System.out.println("hello world");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		
+	
+	}
+	
+}
