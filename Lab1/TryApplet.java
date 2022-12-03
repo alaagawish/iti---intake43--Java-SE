2467c7816dcc83f0b34ff839b1cd8646ae74ef4f
Alaa Elsayed Alaraby <53160167+alaagawish@users.noreply.github.com>
2022-12-03 23:29:05 +0200

    Create TryApplet.java

---
+++ Lab1/TryApplet.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class TryApplet extends Applet{
+	public void paint(Graphics graphics){
+	graphics.drawString("Hello Java", 200, 200);
+	}
+}
